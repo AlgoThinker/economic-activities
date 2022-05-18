0fbf342b003275f6abf51431dd32a16c7365a150
Tushar Sharma <tusharsharma@Tushars-MacBook-Pro.local>
2022-05-18 15:11:20 +0530

    Corrected the content root file path

--- src/main/java/com/nace/economicactivities/util/Constants.java
+++ src/main/java/com/nace/economicactivities/util/Constants.java
@@ -3,6 +3,5 @@ package com.nace.economicactivities.util;
 public class Constants {
     public static final String savedSuccess = "NACE Data saved to DB successfully";
     public static final String unableToSave = "Something went wrong, unable to save data to DB";
-    public static final String filePath = "/Users/tusharsharma/Downloads/economic-activities/src/main/resources/NACE_REV2_20220512_143152.csv";
-
+    public static final String filePath = "src/main/resources/NACE_REV2_20220512_143152.csv";
 }
